9ec96aacdea9b57b87b40f78b573426dd7c28de3
AlfonsoMendeJ <161624131+AlfonsoMendeJ@users.noreply.github.com>
2025-06-23 14:08:10 +0200

    Delete ResultsInfo_driver.cpp

--- ResultsInfo_driver.cpp
+++
@@ -1,9 +0,0 @@
-#include "ResultsInfo_driver.hpp"
-
-// Constructor predeterminado
-ResultsInfo_driver::ResultsInfo_driver()
-    : ResultsInfo(), driver(nullptr) {}
-
-// Constructor con parametros
-ResultsInfo_driver::ResultsInfo_driver(int resultId, const Driver* driver, const Race* race, int grid, int position, int points)
-    : ResultsInfo(resultId, race, grid, position, points), driver(driver) {}
