4c486e0a1cf9a15ff06dc0aeaea15984ef687c78
Emiliodelaplaza <emilio.delaplaza@cunef.edu>
2025-06-23 14:04:16 +0200

    Delete Race.cpp

--- Race.cpp
+++
@@ -1,9 +0,0 @@
-#include "Race.hpp"
-
-// Constructor predeterminado
-Race::Race()
-    : raceId(0), year(0), circuit(nullptr), name(""), date("") {}
-
-// Constructor con parámetros
-Race::Race(int raceId, int year, const Circuit* circuit, std::string name, std::string date)
-    : raceId(raceId), year(year), circuit(circuit), name(name), date(date) {}
